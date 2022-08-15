06ca1d1e288de22c0e3e3111b41b52fa6fc62cd8
Monika <97570594+pp-monika@users.noreply.github.com>
2022-08-15 17:15:46 +0700

    Update
    Add generalization and interactive user input.

--- worldbuilder.py
+++ worldbuilder.py
@@ -1,51 +1,79 @@
+import json
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-# create a MongoClient to the running mongod instance
-client = MongoClient()
-client = MongoClient('localhost', 27017)
 
-# A single instance of MongoDB can support multiple independent databases.
-# Create a database called "Eshasanti". mydatabase now refers to Eshasanti database
-# Automatically created if no database of the name exists.
-eshasanti_db = client['Eshasanti']
+NAME_FIELDS = [ "name", "pronunciation", "meaning", "word formation", "tag", "remark" ]
 
-# Create a collection called "names", which 
-names_col = eshasanti_db['names']
 
-# Get input from user
-name_in = input("Name: ")
-pronunciation_in = input("Pronunciation: ")
-meaning_in = input("Meaning: ")
-word_formation_in = input("Wordformation: ")
-remark_in = input("Remark: ")
-tag_in = input("Tag: ")
-# print("Result")
-# print("Name\t" + name_in)
-# print("Pronunciation\t" + pronunciation_in)
-# print("Meaning\t" + meaning_in)
-# print("Word formation\t" + word_formation_in)
-# print("Remark\t" + remark_in)
-# print("Tag: " + tag_in)
 
-# Create a JSON record from the user input
-record = {
-    "name": name_in,
-    "pronunciation": pronunciation_in,
-    "meaning": meaning_in,
-    "wordFormation": word_formation_in,
-    "remark": remark_in,
-    "tag": tag_in
-}
 
-print(record)
+def toJSON(col_name):
 
-# Insert the JSON record into the collection
-#id = names_col.insert_one(record)
+    record = {}
+    col_fields = []
 
-print("Record count = " + str(names_col.count_documents({})))
-cursor = names_col.find()
-for row in cursor:
-    print(row)
+    # Match the collection to its list of fields
+    if col_name == "names":
+        col_fields = NAME_FIELDS
 
+    for field in col_fields:
+        user_in = input(field + ": ")
+        record[field] = user_in
+
+    return record
+
+
+
+if __name__ == "__main__":
+
+    # create a MongoClient to the running mongod instance
+    client = MongoClient()
+    client = MongoClient('localhost', 27017)
+
+    all_dbs = client.list_database_names()
+    print("All databases")
+    print(all_dbs)
+    working_database = input("Connecting database: ")
+    db = client[working_database]
+
+    all_cols = db.list_collection_names()
+    print("All collections")
+    print(all_cols)
+    working_collection = input("Working collection: ")
+    collection = db[working_collection]
+
+    while True:
+
+        operation = input("Operation: ")
+
+        if operation == "insert":
+            print("Enter record attribute as prompted.")
+            record = toJSON(working_collection)
+            collection.insert_one(record)
+
+        # Doesn't work
+        elif operation == "delete":
+            query = input("Enter query in JSON format: ")
+            db[collection].delete_one(json.loads(query))
+
+        elif operation == "count":
+            print("Record count = " + str(collection.count_documents({})))
+
+        elif operation == "print":
+            cursor = collection.find()
+            for row in cursor:
+                print(row)
+
+        elif operation == "switchdb":
+            working_database = input("Connecting database: ")
+            db = client[working_database]
+
+            working_collection = input("Working collection: ")
+            collection = db[working_collection]
+
+        elif operation == "quit":
+            print("Exit program. Goodbye.")
+            break
+        
 
