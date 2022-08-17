732569942089ed2b421bad9eb90da3f0f826954d
Monika <97570594+pp-monika@users.noreply.github.com>
2022-08-17 17:25:40 +0700

    Upload collectionFieldConst.py
    Upload a collection files constant file. Separated from workdbuilder.py.

---
+++ collectionFieldConst.py
@@ -0,0 +1,8 @@
+# A file containing the list of possible fields for each collection
+
+
+NAME_FIELDS = [ "name", "pronunciation", "meaning", "word formation", "tag", "remark" ]
+
+
+CHARACTER_APPEARANCE = [ "hair", "eyes", "skin", "body type", "cloth", "other" ]
+CHARACTER_FIELDS = [ "name", "age", "gender", "nationality", "occupation", CHARACTER_APPEARANCE, "ability", "personality" ]
