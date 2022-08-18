8280b9b04ac3909978dbbb6eff31ebf2dd473886
Monika <97570594+pp-monika@users.noreply.github.com>
2022-08-18 12:57:27 +0700

    Update collectionFieldConst.py
    - Add more collection fields
    - Start dictionary contain mapping from collection name to a list of all possible fields

--- collectionFieldConst.py
+++ collectionFieldConst.py
@@ -1,8 +1,26 @@
+
+
 # A file containing the list of possible fields for each collection
-
-
-NAME_FIELDS = [ "name", "pronunciation", "meaning", "word formation", "tag", "remark" ]
+NAME_FIELDS = [ "name", "pronunciation", "word formation", "tag", "meaning", "remark" ]
 
 
 CHARACTER_APPEARANCE = [ "hair", "eyes", "skin", "body type", "cloth", "other" ]
-CHARACTER_FIELDS = [ "name", "age", "gender", "nationality", "occupation", CHARACTER_APPEARANCE, "ability", "personality" ]
+CHARACTER_ABILITIES = [ "taat", "satra", "weapon" ]
+CHARACTER_FIELDS = [ "first name", "last name", "age", "gender", "nationality", "occupation", CHARACTER_APPEARANCE, CHARACTER_ABILITIES, "personality" ]
+
+
+SQUAD_INFO = [ "name", "member count", "specialization" ]
+WARRIOR_FIELDS = [ "first name", "last name", "rank", "squad name", ]
+
+
+LOCATION_FIELDS = [ "name", "region", "significance" ]
+
+# Type = Offense, Defense, Support
+SATRA_FIELDS = [ "name", "type", "taat" ]
+
+
+
+
+
+# Dictionary mapping collection name to field list
+COLLECTION_FIELDS_DICT = { "names": NAME_FIELDS }
