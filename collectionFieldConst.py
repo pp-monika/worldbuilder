10cbf477aea4e5e1d6bb0a97baa6a36bcfb50c54
Monika <97570594+pp-monika@users.noreply.github.com>
2022-08-20 12:07:23 +0700

    Update collectionFieldConst.py
    - Modify character nested dictionary

--- collectionFieldConst.py
+++ collectionFieldConst.py
@@ -6,7 +6,7 @@ NAME_FIELDS = [ "name", "pronunciation", "word formation", "tag", "meaning", "re
 
 CHARACTER_APPEARANCE = [ "hair", "eyes", "skin", "body type", "cloth", "other" ]
 CHARACTER_ABILITIES = [ "taat", "satra", "weapon" ]
-CHARACTER_FIELDS = [ "first name", "last name", "age", "gender", "nationality", "occupation", CHARACTER_APPEARANCE, CHARACTER_ABILITIES, "personality" ]
+CHARACTER_FIELDS = [ "first name", "last name", "age", "gender", "nationality", "occupation", "character appearance", "character ability", "personality" ]
 
 
 SQUAD_INFO = [ "name", "member count", "specialization" ]
@@ -23,4 +23,5 @@ SATRA_FIELDS = [ "name", "type", "taat" ]
 
 
 # Dictionary mapping collection name to field list
-COLLECTION_FIELDS_DICT = { "names": NAME_FIELDS }
+COLLECTION_FIELDS_DICT = { "names": NAME_FIELDS, "characters": CHARACTER_FIELDS, 
+                            "character appearance": CHARACTER_APPEARANCE, "character ability": CHARACTER_ABILITIES }
